e37a458df08369ec451081c5fdf6a3ec07738c3d
ch85uwtcss <ch85@uw.edu>
2017-02-02 17:53:02 -0800

    Initial commit for my branch

---
+++ AbstractController.java
@@ -0,0 +1,19 @@
+public class AbstractController {
+	public String viewMyJobs(String userName){
+		return "foo";
+	}
+	
+	public Job FindJobs() {
+		return new Job();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Volunteer.java
@@ -0,0 +1,1 @@
+public class Volunteer extends AbstractUser {}
