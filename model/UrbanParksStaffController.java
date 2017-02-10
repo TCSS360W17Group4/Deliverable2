036ca622a25a7dd0dceeef7dae55bcc7bd000e85
anthonyrichardson <tony48@uw.edu>
2017-02-10 14:06:42 -0800

    comment

--- model/UrbanParksStaffController.java
+++ model/UrbanParksStaffController.java
@@ -1,4 +1,3 @@
-package model;
 /* 
    extending system would make accessing data in system easy
    but extending to share data makes the object too bound to system
@@ -11,6 +10,7 @@ package model;
  * 
  */
 
+package model;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,12 +19,25 @@ import java.util.ArrayList;
 public class UrbanParksStaffController extends AbstractController{
     
     private final List<Job> myJobs;
-    private final List<Volunteer> myVolunteers;
-    private final List<ParkManager> myParkManagers;
+	private final List<Volunteer> myVolunteers;
+	private final List<ParkManager> myParkManagers;
     private final List<UrbanParksStaff> myUrbanParksStaff;
     private JobController myJobController;
     
-    public UrbanParksStaffController(List<Job> theJobs, 
+    
+    /**
+     * Constructor for UrbanParksStaffController
+     * 
+     * 
+     * @param theJobs The list of jobs for the park system.
+     * @param theVolunteers The list of Volunteers for the park system.
+     * @param theParkManagers The list of Park Managers for the park system.
+     * @param theUrbanParksStaff The list of Urban Parks Staff for the park system.
+     * @param theJobController The job controller from the parks system.
+     * 
+     */
+    public UrbanParksStaffController(
+    List<Job> theJobs, 
     List<Volunteer> theVolunteers, 
     List<ParkManager> theParkManagers, 
     List<UrbanParksStaff> theUrbanParksStaff, 
@@ -38,19 +51,35 @@ public class UrbanParksStaffController extends AbstractController{
     
     
     // User story 5: As an Urban Parks staff member I want to change the maximum number of pending jobs that are allowed in the system.
+    /**
+     * Changes the maximum number of pending jobs for the park system.
+     * 
+     * @param newMaxJobs The new maximum number of jobs for the park system.
+     */
     public void changeMaxJobs(int newMaxJobs) {
         // uses the setter method in JobController class
         myJobController.setMyMaxNumberOfPendingJobs(newMaxJobs);
-    }  
+    }
+    
     
     
     // User story 4: As an Urban Parks staff member I want to view a one-month calendar of all upcoming jobs.
     // view one month calendar
     // returns a list of upcoming jobs
+    /**
+     * Returns a list of jobs for the next month.
+     * 
+     * 
+     * @return A list of pending jobs with start dates from now to one month from now.
+     */
     public List<Job> viewCalendar() {
         List<Job> pendingJobs = new ArrayList<Job>();
         for (Job j : myJobs) {
-            if (j.getMyJobIsPending()) {
+            if (j.getMyJobIsPending() && 
+                // compares start date to current date right now
+                j.getMyStartDate() > j.getMyStartDate().now() &&
+                // compares start date to date of one month from right now
+                j.getMyStartDate() < j.getMyStartDate().now().plusMonths(1)) {
                 pendingJobs.add(j);
             }
         }
@@ -58,62 +87,5 @@ public class UrbanParksStaffController extends AbstractController{
     }
     
     
-    
-    
-    /*
-    private Job[] viewCalendar(int startDate, int endDate) {
-        // sorts job array if not already sorted
-        
-    } 
-    
-    
-    private Job[] viewCalendar(int startDate, int endDate) {
-        // sorts job array if not already sorted
-        sortJobsByStartDate(jobs);
-        // get indicies in array jobs
-        
-        int s = 0;
-        int e = jobs.length();
-        int check = (s + e)/2;
-        while(jobs[check].myStartDate != startDate ||
-              e - s > 1) {
-            if(jobs[check].myStartDate > s) {
-                s = check;
-            }
-            if(jobs[check].myStartDate < e) {
-                e = check;
-            }
-        }
-        // check = index
-        
-        
-        //find index 
-        int d = jobs.length()/2;
-        int len = jobs.length()/4;
-        while(d != startdate && len != 0) {
-            if(d > startdate) {
-                d -= len;
-            } else if(d < startdate) {
-                d += len;
-            }
-            len /= 2;
-        }
-       
-        
-        int s = binarySearch(jobs, startDate);
-        int e = binarySearch(jobs, endDate);
-        
-        return Arrays.copyOfRange(jobs, s, e);
-    } 
-    
-    private int binarySearch(Job[] arr, int date) {
-        
-        int s = 0;
-        int e = arr.length();
-        int check = 0;
-        while(arr[check].) {
-            
-        }
-    }
-    */
+
 }
