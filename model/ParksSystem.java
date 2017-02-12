061e200b4e5e5003691d9a92e1c0dd6bd3d726ac
Dere De <dere.eclipsed@gmail.com>
2017-02-11 20:22:44 -0800

    updated loginSuccessful-test passed

--- model/ParksSystem.java
+++ model/ParksSystem.java
@@ -280,53 +280,62 @@ public class ParksSystem implements java.io.Serializable{
 	public boolean loginSuccessful(String theUserName) {
 		//parse the user name
 		String userType = theUserName.substring(0,3);
+		//can be index 3, since username has length 4 generally
 		String userId = theUserName.substring(3,theUserName.length());
 		boolean isSuccessful = false;
+	
+		int id;
 		
-		//check if user type exist and user id is an int
-		if(UserType.userExist(userType) && userId.matches("[0-9]+")) {
-				int id = Integer.parseInt(userId);
+		if (UserType.userExist(userType) && userId.matches("[0-9]+")) {
+			id = Integer.parseInt(userId);
+		} else {
+			return false;//no need to check the other statements
+		}
 				
-				
-				if(userType.equals(UserType.Volunteer.getMyType()) ) {
+				//id check to avoid nullpointer calling Contains
+		if(userType.equals(UserType.Volunteer.getMyType()) 
+				&& id < myVolunteers.size()
+				&& myVolunteers.contains(myVolunteers.get(id)) ) {
 				    myUserController = new VolunteerController(
 				            (Volunteer)myCurrentUser, 
 				            myVolunteers, 
 				            myParkManagers, 
 				            myUrbanStaff, 
 				            myJobController);
-				    isSuccessful = true;
-				} else if(userType.equals(UserType.Manager.getMyType()) ) {
+		    isSuccessful = true;
+		} else if(userType.equals(UserType.Manager.getMyType())
+				&& id < myParkManagers.size()
+				&& myParkManagers.contains(myParkManagers.get(id)) ) {
 		             myUserController = new ParkManagerController(
 	                        (ParkManager)myCurrentUser, 
 	                        myVolunteers, 
 	                        myParkManagers, 
 	                        myUrbanStaff, 
 	                        myJobController);
-		             isSuccessful = true;
-				} else if (userType.equals(UserType.Staff.getMyType()) ) {
-				    /*
-				    myUserController = new UrbanParksStaffController(
-	                        (UrbanParksStaff)myCurrentUser, 
-	                        myVolunteers, 
-	                        myParkManagers, 
-	                        myUrbanStaff, 
-	                        myJobController);
-	                        */
-			    	isSuccessful = true;
-				 } else {
-				//user doesnt exist
-					 isSuccessful = false;
-				 }
-		}
+             isSuccessful = true;
+		} else if (userType.equals(UserType.Staff.getMyType()) 
+				&& id < myUrbanStaff.size()
+				&& myUrbanStaff.contains(myUrbanStaff.get(id)) ) {
+		    /*
+		    myUserController = new UrbanParksStaffController(
+                    (UrbanParksStaff)myCurrentUser, 
+                    myVolunteers, 
+                    myParkManagers, 
+                    myUrbanStaff, 
+                    myJobController);
+                    */
+	    	isSuccessful = true;
+		 } else {
+		//user doesnt exist
+			 isSuccessful = false;
+		 }
+		
 		
 		return isSuccessful;
 			
 	}
 	
-	public void logout() {
-		
-	}
+	
 	
 	
 	/**
@@ -351,5 +360,32 @@ public class ParksSystem implements java.io.Serializable{
 	    //Also needed after we sort out JUnit testing
 	    //myUserController.run();  
 	}
+
+	public List<ParkManager> getMyParkManagers() {
+		return myParkManagers;
+	}
+
+	public void setMyParkManagers(List<ParkManager> theParkManagers) {
+		ParksSystem.myParkManagers = theParkManagers;
+	}
 	
+	public  List<Volunteer> getMyVolunteers() {
+		return myVolunteers;
+	}
+
+	public  void setMyVolunteers(List<Volunteer> theVolunteers) {
+		ParksSystem.myVolunteers = theVolunteers;
+	}
+
+	public static List<UrbanParksStaff> getMyUrbanStaff() {
+		return myUrbanStaff;
+	}
+
+	public void setMyUrbanStaff(List<UrbanParksStaff> theUrbanStaff) {
+		ParksSystem.myUrbanStaff = theUrbanStaff;
+	}
+
+	public void logout() {
+		
+	}
 }
