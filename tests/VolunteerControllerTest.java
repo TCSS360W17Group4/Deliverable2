d0604032d23bb1e6ce2b75c2c3c227e7d9c9814b
Dere De <dere.eclipsed@gmail.com>
2017-02-28 07:53:34 -0800

    more test for volunteerController

--- tests/VolunteerControllerTest.java
+++ tests/VolunteerControllerTest.java
@@ -20,11 +20,14 @@ public class VolunteerControllerTest {
 
 	private int JOB_ID_WITH_NO_CONFLICTING_DATE = 0;
 	private int JOB_ID_WITH_CONFLICTING_DATE_AS_JOB_ID_ONE = 1;
+	private int VOLUNTEER_ID = 0;
 	private int INVALID_JOB_ID_NEGATIVE_VALUE = -1;
+	private int NON_ASSIGNED_JOB_ID = 2;
 	private Job nonConflictingJob;
 	private Job conflictingJob;
 	private Volunteer volunteer;
 	private VolunteerController controller;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -45,7 +48,7 @@ public class VolunteerControllerTest {
 		nonConflictingJob.setMyEndDate(firstJobEndDate);
 		
 		volunteer = new Volunteer();
-		
+		volunteer.setMyUserId(VOLUNTEER_ID);
 		/*set up non conflicting job that could be added with no issue*/
 		conflictingJob = new Job(new Park());
 		conflictingJob.setMyJobId(JOB_ID_WITH_CONFLICTING_DATE_AS_JOB_ID_ONE);
@@ -111,4 +114,173 @@ public class VolunteerControllerTest {
 				controller.hasJobViolateMaxJobPerDayPerVolunteer(INVALID_JOB_ID_NEGATIVE_VALUE);
 	}
 	
+	/**
+	 * @author Dereje Bireda
+	 * 
+	 *{@link model.VolunteerController#hasJobViolateMaxJobPerDayPerVolunteer(int)}
+	 */
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void hasJobViolatedMaxJobPerDayPerVolunteer_Adding_Non_Assigned_JobId() {
+		volunteer.getMyVolunteerJobs().add(JOB_ID_WITH_NO_CONFLICTING_DATE);
+			controller.hasJobViolateMaxJobPerDayPerVolunteer(NON_ASSIGNED_JOB_ID);
+	}
+	/**
+	 * @author Dereje Bireda
+	 * 
+	 *{@link model.VolunteerController#hasMinSignupDaysBeforeJobStartPassed(Job)}
+	 */
+	
+	@Test
+	public void hasMinSignupDaysBeforeJobStartPassed_Signup_When_Job_IsPast() {
+		Job pastJob = new Job(new Park());
+		
+		LocalDate startDate = JobController.convertStringToDate("02/10/17");
+		LocalDate endDate = JobController.convertStringToDate("02/13/17");
+	
+		pastJob.setMyStartDate(startDate);
+		pastJob.setMyEndDate(endDate);
+		
+		
+		assertTrue("Fail! signed up for a past job", controller.hasMinSignupDaysBeforeJobStartPassed(pastJob));
+		
+		
+	}
+	
+	
+	/**
+	 * @author Dereje Bireda
+	 * 
+	 *{@link model.VolunteerController#hasMinSignupDaysBeforeJobStartPassed(Job)}
+	 */
+	
+	@Test
+	public void hasMinSignupDaysBeforeJobStartPassed_Job_Has_MoreThan_Min_Days_For_Signup() {
+		Job futureJobStillOpen = new Job(new Park());
+		
+		LocalDate startDate = JobController.convertStringToDate("03/02/17");
+		LocalDate endDate = JobController.convertStringToDate("03/03/17");
+	
+		futureJobStillOpen.setMyStartDate(startDate);
+		futureJobStillOpen.setMyEndDate(endDate);
+		
+		
+		assertFalse("Fail! sign up for still open job", controller.hasMinSignupDaysBeforeJobStartPassed(futureJobStillOpen));
+		
+		
+	}
+	
+	/**
+	 * @author Dereje Bireda
+	 * 
+	 *{@link model.VolunteerController#hasMinSignupDaysBeforeJobStartPassed(Job)}
+	 */
+	
+	@Test
+	public void hasMinSignupDaysBeforeJobStartPassed_Job_Start_Date_Below_Min_Days_For_Signup() {
+		Job futureJobClosed = new Job(new Park());
+		
+		LocalDate startDate = JobController.convertStringToDate("02/29/17");
+		LocalDate endDate = JobController.convertStringToDate("02/29/17");
+	
+		futureJobClosed.setMyStartDate(startDate);
+		futureJobClosed.setMyEndDate(endDate);
+		
+		
+		assertTrue("Fail! sign up for future job, but sign up date passed",
+				controller.hasMinSignupDaysBeforeJobStartPassed(futureJobClosed));
+		
+		
+	}
+	
+	/**
+	 * @author Dereje Bireda
+	 * 
+	 *{@link model.VolunteerController#isJobFullForSignUp(Job)}
+	 */
+	@Test
+	public void isJobFullForSignUp_Job_Already_Has_Max_Volunteer_Allowed() {
+		Job fullJob = new Job(new Park());
+		int numberOfVolunteersRequested = 2;
+		int numberOfCurrentVolunteers = 2;
+		fullJob.setMyLightVolunteerNumber(numberOfVolunteersRequested);
+		fullJob.setMyMediumVolunteerNumber(0);
+		fullJob.setMyHeavyVolunteerNumber(0);
+		fullJob.setMyCurrentTotalVolunteers(numberOfCurrentVolunteers);
+
+		assertTrue("Fail!Job already has max number of volunteer requested",
+					controller.isJobFullForSignUp(fullJob));
+	}
+	
+	/**
+	 * @author Dereje Bireda
+	 * 
+	 *{@link model.VolunteerController#isJobFullForSignUp(Job)}
+	 */
+	@Test
+	public void isJobFullForSignUp_Job_Has_One_Spot_Tofill() {
+		Job fullJob = new Job(new Park());
+		//volunteers number with two cases
+		int numberOfVolunteersRequested = 2;
+		int numberOfCurrentVolunteers = 1;
+		
+		fullJob.setMyLightVolunteerNumber(numberOfVolunteersRequested);
+		fullJob.setMyMediumVolunteerNumber(0);
+		fullJob.setMyHeavyVolunteerNumber(0);
+		fullJob.setMyCurrentTotalVolunteers(numberOfCurrentVolunteers);
+
+		assertFalse("Fail!Job has one spot left",
+					controller.isJobFullForSignUp(fullJob));
+	}
+	
+	
+	/**
+	 * @author Dereje Bireda
+	 * 
+	 *{@link model.VolunteerController#isJobFullForSignUp(Job)}
+	 */
+	@Test(expected = NullPointerException.class)
+	public void isJobFullForSignUp_Null_Passed_As_Parameter() {
+
+		assertFalse("Fail!Job is Null",
+					controller.isJobFullForSignUp(null));
+	}
+	
+
+	/**
+	 * @author Dereje Bireda
+	 * 
+	 *{@link model.VolunteerController#volunteerHasTheJob(Job)}
+	 */
+	@Test
+	public void volunteerHasTheJob_Job_Belongs_To_Volunteer() {
+		
+		int jobId = 1;
+		Job volunteerJob = new Job(new Park());
+		volunteerJob.setMyJobId(jobId);
+		//add volunteer to job object
+		volunteer.setMyUserId(VOLUNTEER_ID);
+		volunteerJob.getMyVolunteerList().add(VOLUNTEER_ID);
+		//add job to volunteer
+		volunteer.getMyVolunteerJobs().add(jobId);
+		assertTrue("Fail!volunteer already signed up for the job",
+					controller.volunteerHasTheJob(jobId));
+	}
+	
+	
+	/**
+	 * @author Dereje Bireda
+	 * 
+	 *{@link model.VolunteerController#volunteerHasTheJob(Job)}
+	 */
+	@Test
+	public void volunteerHasTheJob_Volunteer_First_Time_Signup_For_TheJob() {
+		
+		int jobId = 1;
+		Job newJob = new Job(new Park());
+		newJob.setMyJobId(jobId);
+		//add volunteer to job object
+		volunteer.setMyUserId(VOLUNTEER_ID);
+	
+		assertFalse("Fail!Volunteer does not have the job",controller.volunteerHasTheJob(jobId));
+	}
 }
